563d98c93db4304941d12ec15a58f287ab8ba343
Hanna Babai <kot.vmeshke.4@gmail.com>
2025-07-19 14:53:30 +0200

    feat: add header

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,43 @@
-console.log('Hello, QRT!')
+console.log('Hello, QRT!');
+
+const handleBurgerMenu = () => {
+  const wrapper = document.querySelector('.header-nav-wrapper');
+  const burgerButton = wrapper.querySelector('.header-burger');
+  const headerNav = wrapper.querySelector('.header-nav');
+  const navLinks = headerNav.querySelectorAll('.header-nav-link');
+
+  const openMenu = () => {
+    wrapper.classList.add('js-opened');
+    document.addEventListener('click', handleOutsideClick);
+  };
+
+  const closeMenu = () => {
+    wrapper.classList.remove('js-opened');
+    document.removeEventListener('click', handleOutsideClick);
+  };
+
+  const toggleMenu = () => {
+    wrapper.classList.contains('js-opened') ? closeMenu() : openMenu();
+  };
+
+  const handleOutsideClick = (e) => {
+    if (!wrapper.contains(e.target)) {
+      closeMenu();
+    }
+  };
+
+  burgerButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    toggleMenu();
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleBurgerMenu();
+});
